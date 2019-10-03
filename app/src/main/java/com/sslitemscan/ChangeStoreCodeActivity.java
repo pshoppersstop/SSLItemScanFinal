d2e00cfa228d0d53bd0513d459dd2e92eabdb780
pshoppersstop <data@123>
2019-10-03 15:27:15 +0530

    Change Store Code Implemented

--- app/src/main/java/com/sslitemscan/ChangeStoreCodeActivity.java
+++ app/src/main/java/com/sslitemscan/ChangeStoreCodeActivity.java
@@ -1,11 +1,20 @@
 package com.sslitemscan;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.sslitemscan.Constants.Constants;
+import com.sslitemscan.global.PreferencesManager;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ChangeStoreCodeActivity extends AppCompatActivity {
 
@@ -18,9 +27,31 @@ public class ChangeStoreCodeActivity extends AppCompatActivity {
     @BindView(R.id.button_save)
     Button mButtonSaveDetails;
 
+    public static Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_store_code);
+        mContext = this;
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.button_save)
+    public void changeStoreId(){
+
+        if( !TextUtils.isEmpty(mETStoreCode.getText().toString()) && !(TextUtils.isEmpty(mETReEnterStoreCode.getText().toString())) ){
+
+            if (  (mETStoreCode.getText().toString()).equals(mETReEnterStoreCode.getText().toString())  ){
+                PreferencesManager.getInstance().setString(Constants.STOREID,mETStoreCode.getText().toString());
+                Toast.makeText(mContext,"Store Code Changed Sucessfully",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(mContext,"Store Code Should Be Same",Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(mContext,"Store Code Should Not Be Empty",Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
