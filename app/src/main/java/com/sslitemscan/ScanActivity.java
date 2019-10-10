be1c265d202ba5ea0f6d14a09e2e8726459d7720
pshoppersstop <data@123>
2019-10-10 17:25:38 +0530

    issue fixed regarding MAE-2721-

--- app/src/main/java/com/sslitemscan/ScanActivity.java
+++ app/src/main/java/com/sslitemscan/ScanActivity.java
@@ -156,7 +156,7 @@ public class ScanActivity extends AppCompatActivity implements BarcodeReader.Bar
                 .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Log.e("DeleteProductCode","****"+productCode);
+                        Log.e("DeleteProductCode","******"+productCode);
                         barcodeReader.resumeScanning();
                         dialog.dismiss();
                     }
